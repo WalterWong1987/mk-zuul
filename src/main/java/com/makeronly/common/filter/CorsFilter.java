2b7d7c7eaff72f59910b07550a6100c7c89b75f8
WalterWong1987 <walterwong1987@gmail.com>
2017-11-13 14:48:25 +0800

    服务网关

---
+++ src/main/java/com/makeronly/common/filter/CorsFilter.java
@@ -0,0 +1,31 @@
+package com.makeronly.common.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+/**
+ * 跨域过滤器
+ * @author Walter Wong
+ */
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        /**RestCorsFilter
+         * 允许的Header值，不支持通配符
+         */
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        /**
+         * 即使只用其中几种，header和options是不能删除的，因为浏览器通过options请求来获取服务的跨域策略
+         */
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        /**
+         * CORS策略的缓存时间
+         */
+        responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
---
+++ src/main/java/com/makeronly/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.makeronly.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * API Service Gateway
+ * @author Walter Wong
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
